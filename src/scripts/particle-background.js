32d08ffb50c6e321165608d31d787d3b3f7e3987
Julien Look <julien.look@cinoware.com>
2024-10-31 12:56:42 +0100

    fixed tsparticles, isolated sample particle script

--- src/scripts/particle-background.js
+++ src/scripts/particle-background.js
@@ -3,98 +3,96 @@
 
 const particlesConfig = {
   /* your tsParticles configuration */
-  emitters: [],
 };
-$(document).ready(async function () {
-  await loadFull(tsParticles, particlesConfig);
 
+$(document).ready(async function () {
   // if we have a mobile browser we will disable the repulse function
   let windowHeight = window.innerHeight;
   let windowWidth = window.innerWidth;
   let windowRatio = windowWidth / windowHeight;
 
-  $('#tsparticles')
-    .particles()
-    .init(
-      {
-        fpsLimit: 60,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: 'push',
-            },
-            onHover: {
-              enable: windowRatio > 1.2 ? true : false,
-              mode: 'repulse',
-            },
-            resize: true,
+  tsParticles.load(
+    'tsparticles',
+    {
+      emitters: [],
+      fpsLimit: 60,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: 'push',
           },
-          modes: {
-            push: {
-              quantity: 40,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
+          onHover: {
+            enable: windowRatio > 1.2 ? true : false,
+            mode: 'repulse',
+          },
+          resize: true,
+        },
+        modes: {
+          push: {
+            quantity: 40,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
           },
         },
-        particles: {
-          color: {
-            value: '#ffffff',
-          },
-          links: {
-            color: '#ffffff',
-            distance: 150,
-            enable: true,
-            opacity: 0.3,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: 'none',
-            enable: true,
-            outModes: {
-              default: 'bounce',
-            },
-            random: false,
-            speed: 3,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 60,
-            limit: 250,
-          },
-
-          opacity: {
-            value: 0.3,
-          },
-          shape: {
-            type: 'circle',
-          },
-          size: {
-            value: { min: 1, max: 2 },
-          },
-        },
-        detectRetina: true,
       },
-      function (container) {
-        // container is the particles container where you can play/pause or stop/start.
-        // the container is already started, you don't need to start it manually.
-        setInterval(() => {
-          // clear particles every 100ms to prevent overloaded background
-          if (container.particles._array.length > 40) {
-            container.particles._array.splice(-1, 20);
-            container.particles._zArray.splice(-1, 20);
-          }
-        }, 100);
-      }
-    );
+      particles: {
+        color: {
+          value: '#ffffff',
+        },
+        links: {
+          color: '#ffffff',
+          distance: 150,
+          enable: true,
+          opacity: 0.3,
+          width: 1,
+        },
+        collisions: {
+          enable: true,
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outModes: {
+            default: 'bounce',
+          },
+          random: false,
+          speed: 3,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 60,
+          limit: 250,
+        },
+
+        opacity: {
+          value: 0.3,
+        },
+        shape: {
+          type: 'circle',
+        },
+        size: {
+          value: { min: 1, max: 2 },
+        },
+      },
+      detectRetina: true,
+    },
+    function (container) {
+      // container is the particles container where you can play/pause or stop/start.
+      // the container is already started, you don't need to start it manually.
+      setInterval(() => {
+        // clear particles every 100ms to prevent overloaded background
+        if (container.particles._array.length > 40) {
+          container.particles._array.splice(-1, 20);
+          container.particles._zArray.splice(-1, 20);
+        }
+      }, 100);
+    }
+  );
 });
